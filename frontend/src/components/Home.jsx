b7fb14e26af5572259700babd1ad479a10cd7a69
Gulrez Alam <egulrezalam@gmail.com>
2024-12-14 01:21:38 +0530

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -80,7 +80,7 @@ const Home = () => {
 
   const fetchUserRole = async () => {
     try {
-      const response = await axios.get("http://localhost:5000/api/user/role", {
+      const response = await axios.get("https://sih-project-xtmx.onrender.com/api/user/role", {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
